910d55cb592b03c5b320186235a3ca2e1a58e097
Eriknicklasson <62430975+Eriknicklasson@users.noreply.github.com>
2021-05-07 20:19:14 +0200

    Godkänt test

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -36,6 +36,6 @@ test('if elemnt adds to top of stack', async () => {
   let knapp = await driver.findElement(By.id('peek'));
   await knapp.click();
   let stack = await driver.findElement(By.id('top_of_stack')).getText();
-  experct(stack).toEqual("jkhgigkhgkh");
+  experct(stack).toEqual("Bananer");
 
 });
