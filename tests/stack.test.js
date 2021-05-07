60adf43eb7473d1a9af59c4080f8b6ca891ca39f
Eriknicklasson <62430975+Eriknicklasson@users.noreply.github.com>
2021-05-07 13:29:44 +0200

    felaktigt test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,8 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('If pop empty', () => {
+    stack.push(0);
+    expect(stack.pop()).toBeUndefined();
+});
